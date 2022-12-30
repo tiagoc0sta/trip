ac0529d5dcf25a074bdac2350c7015f354f49498
Tiago Costa <tiagodpv@gmail.com>
2022-12-30 00:17:07 -0500

    final version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Activities from './components/Activities';
+import Booking from './components/Booking';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Gallery from './components/Gallery';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Topbar from './components/Topbar';
@@ -11,6 +15,10 @@ function App() {
       <Navbar />
       <Hero />
       <Activities />
+      <Booking />
+      <Gallery />
+      <Contact />
+      <Footer />
     </div>
   );
 }
